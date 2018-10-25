0863c16e354315dfb0a98dfbde3e3fe927dbbc0f
Lester Ng Kim Ho <lesterng94@gmail.com>
2018-10-26 02:29:59 +0800

    Added new shared UI files into UnicabApp

--- UnicabApp/UnicabApp/UnicabApp/App.xaml.cs
+++ UnicabApp/UnicabApp/UnicabApp/App.xaml.cs
@@ -11,7 +11,7 @@ namespace UnicabApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new Landing.MainPage());
         }
 
         protected override void OnStart()
--- UnicabApp/UnicabApp/UnicabApp/Landing/MainPage.xaml.cs
+++ UnicabApp/UnicabApp/UnicabApp/Landing/MainPage.xaml.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace UnicabApp
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UnicabApp.Landing
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : TabbedPage
     {
-        public MainPage()
+        public MainPage ()
         {
             InitializeComponent();
         }
