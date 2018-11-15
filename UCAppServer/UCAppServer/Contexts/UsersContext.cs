99a4c94785c5c7a017b0703aa6e2e96ddeea54dd
Lester Ng Kim Ho <lesterng94@gmail.com>
2018-11-15 10:35:21 +0800

    Configure app server connection to backing SQL Server instance

---
+++ UCAppServer/UCAppServer/Contexts/UsersContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+using UCAppServer.Models;
+
+namespace UCAppServer.Contexts
+{
+    public class UsersContext : DbContext
+    {
+        public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+
+        public DbSet<Driver> Drivers { get; set; }
+
+        public DbSet<Passenger> Passengers { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
