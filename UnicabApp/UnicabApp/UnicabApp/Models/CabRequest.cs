ad8a7fab91f271a531b220069c6e11f37699b08b
Lester Ng Kim Ho <lesterng94@gmail.com>
2018-11-19 11:09:09 +0800

    Add CabRequest modal; update RequestCab page

---
+++ UnicabApp/UnicabApp/UnicabApp/Models/CabRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnicabApp.Models
+{
+    public class CabRequest
+    {
+        public string PickupLocation { get; set; }
+
+        public string DropoffLocation { get; set; }
+
+        public DateTime PickupDate { get; set; }
+
+        public TimeSpan PickupTime { get; set; }
+
+        public int NoOfPassengers { get; set; }
+
+        public bool LadiesOnly { get; set; }
+
+        public string AdditionalNotes { get; set; }
+    }
+}
--- UnicabApp/UnicabApp/UnicabApp/Passenger/RequestCabPage.xaml.cs
+++ UnicabApp/UnicabApp/UnicabApp/Passenger/RequestCabPage.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using UnicabApp.Models;
+
 namespace UnicabApp.Passenger
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
@@ -16,5 +18,31 @@ namespace UnicabApp.Passenger
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private async void BookCabBtn_Clicked(object sender, EventArgs e)
+        {
+            string confirmBookingText = string.Format("Do you wish to confirm your cab request as follows:\n\nPickup Location: {0}\nDropoff Location: {1}\nPickup Date: {2:D}\nPickup Time: {3}\nNo. of Passengers: {4}\nLadies Only?: {5}\nAdditional Notes: {6}",
+                PickupLocationPicker.SelectedItem,
+                DropoffLocationPicker.SelectedItem,
+                PickupDatePicker.Date.ToString("ddd, d MMM yyyy"),
+                DateTime.Today.Add(PickupTimePicker.Time).ToString("h:mm tt"),
+                NoOfPassengersPicker.SelectedItem,
+                LadiesOnlyPicker.SelectedItem,
+                AdditionalNotesEditor.Text);
+
+            bool confirmBooking = await DisplayAlert("Request Cab", confirmBookingText, "Yes", "No");
+
+            if (confirmBooking)
+            {
+                // add operations to book a cab
+                await DisplayAlert("Request Cab", "Your cab request is being processed. You will be notified once it is accepted by any of our drivers.", "OK");
+                
+            }
+            else
+            {
+                await DisplayAlert("Request Cab", "You have cancelled your cab request.", "OK");
+            }
+        }
+
+    }
 }
