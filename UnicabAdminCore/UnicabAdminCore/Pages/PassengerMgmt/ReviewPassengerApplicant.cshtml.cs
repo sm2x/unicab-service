dfe0dc4695d5881b2f4c11fb4d06f83d4561d8f5
Lester Ng Kim Ho <lesterng94@gmail.com>
2018-12-07 00:45:13 +0800

    Driver and passenger admin interface development

--- UnicabAdminCore/UnicabAdminCore/Pages/PassengerMgmt/ReviewPassengerApplicant.cshtml.cs
+++ UnicabAdminCore/UnicabAdminCore/Pages/PassengerMgmt/ReviewPassengerApplicant.cshtml.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using UnicabAdminCore.Models;
+using UnicabAdminCore.Services;
 
 namespace UnicabAdminCore.Pages.PassengerMgmt
 {
     public class ReviewPassengerApplicantModel : PageModel
     {
-        public void OnGet()
-        {
+        private IPassengerManagementService passengerManagementService;
+        public PassengerApplicant passengerApplicant;
 
+        public ReviewPassengerApplicantModel(IPassengerManagementService service)
+        {
+            passengerManagementService = service;
+        }
+
+        public async Task OnGetAsync(int id)
+        {
+            passengerApplicant = await passengerManagementService.GetPassengerApplicant(id);
         }
     }
 }
